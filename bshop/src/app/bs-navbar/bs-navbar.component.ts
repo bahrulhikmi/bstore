1775a57b10601918d2eea33965c1d419b1bf67ed
arul <bahrulhikmi@gmail.com>
2021-04-25 01:45:27 +0800

    added cart count

--- bshop/src/app/bs-navbar/bs-navbar.component.ts
+++ bshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,23 +1,37 @@
-import { Component} from '@angular/core';
-import {Observable} from 'rxjs'
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs'
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent  {
+export class BsNavbarComponent implements OnInit {
 
-appUser: AppUser;
+  appUser: AppUser;
+  shoppingCartItemCount: number;
 
-constructor(private auth: AuthService) { 
-  auth.appUser$.subscribe(appUser => this.appUser = appUser);
-}
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
+  }
 
-logout(){
-  this.auth.logout();
-}
+  logout() {
+    this.auth.logout();
+  }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      console.log(cart);
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    });
+
+  }
 
 }
--- bshop/src/app/shopping-cart.service.ts
+++ bshop/src/app/shopping-cart.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 import { Cart } from './models/cart';
 import { Product } from './models/product';
+import { ShoppingCart } from './models/shopping-cart';
 
 @Injectable({
   providedIn: 'root'
@@ -11,43 +13,43 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  private create(){
+  private create() {
     return this.db.list('/shopping-carts').push({
       dataCreated: new Date().getTime()
     });
   }
 
-  async getCart(){
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/'+ cartId).valueChanges();
+    return this.db.object('/shopping-carts/' + cartId).valueChanges().pipe(map(a => <ShoppingCart>a));
   }
 
-  private getItem(cartId: string, productId: string){
-    return this.db.object('/shopping-carts/'+cartId+'/items/'+productId);
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
-  private async getOrCreateCartId(): Promise<string>{
+  private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem('cartId');
-    if(cartId) return cartId;
+    if (cartId) return cartId;
 
-    let result = await this.create();    
+    let result = await this.create();
     localStorage.setItem('cartId', result.key)
-    return result.key;                     
+    return result.key;
   }
 
-  async addToCart(product: Product){
+  async addToCart(product: Product) {
     this.updateItemQuantity(product, 1);
   }
 
-  async removeFromCart(product: Product){
+  async removeFromCart(product: Product) {
     this.updateItemQuantity(product, -1);
   }
 
-  private async updateItemQuantity(product:Product, change:number){
-    let cartId = await this.getOrCreateCartId();    
+  private async updateItemQuantity(product: Product, change: number) {
+    let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.key);
-    item$.valueChanges().pipe(take(1)).subscribe(item=>{        
-      item$.update({product: product, quantity: ( ((<Cart>item) && (<Cart>item).quantity) || 0 ) + change});
+    item$.valueChanges().pipe(take(1)).subscribe(item => {
+      item$.update({ product: product, quantity: (((<Cart>item) && (<Cart>item).quantity) || 0) + change });
     });
   }
 }
